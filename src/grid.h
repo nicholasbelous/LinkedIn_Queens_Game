fabc07c093b9b74b13539af434a80ab4fd994da6
n-belous <118130829+n-belous@users.noreply.github.com>
2026-04-25 18:13:31 -0400

    Completed grid header file

---
+++ src/grid.h
@@ -0,0 +1,21 @@
+#ifndef GRID_H
+#define GRID_H
+#include <vector>
+
+class Grid {
+private: 
+    int size;
+    std::vector<std::vector<int>> board;
+    std::vector<std::vector<bool>> crown;
+
+public:
+    Grid(std::vector<std::vector<int>> b, int s);
+    Grid(std::vector<std::vector<int>> b);
+
+    bool placeCrown(int row, int col);
+    bool isValid();
+    void print();
+};
+
+
+#endif //GRID_H
